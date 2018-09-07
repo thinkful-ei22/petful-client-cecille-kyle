6bd8d6a8e9617466699f924f319ecc9ff0cf7444
Kyle Bowen <kyle.m.bowen@gmail.com>
2018-09-07 11:52:35 -0600

    flesh out reducers

--- src/reducers/cat.js
+++ src/reducers/cat.js
@@ -1,3 +1,12 @@
+import {
+  ADOPT_CAT_ERROR,
+  ADOPT_CAT_SUCCESS,
+  ADOPT_CAT_REQUEST,
+  FETCH_CAT_ERROR,
+  FETCH_CAT_SUCCESS,
+  FETCH_CAT_REQUEST
+} from '../actions/cat';
+
 const initialState = {
   data: null,
   error: null,
@@ -5,5 +14,47 @@ const initialState = {
 };
 
 export const catReducer = (state=initialState, action) => {
-  return state;
+  switch (action.type) {
+    case FETCH_CAT_REQUEST: {
+      return Object.assign({}, state, {
+        error: null,
+        loading: true
+      });
+    }
+    case FETCH_CAT_SUCCESS: {
+      return Object.assign({}, state, {
+        data: action.cat,
+        error: null,
+        loading: false
+      })
+    }
+    case FETCH_CAT_ERROR: {
+      return Object.assign({}, state, {
+        error: action.error,
+        loading: false
+      })
+    }
+    case ADOPT_CAT_REQUEST: {
+      return Object.assign({}, state, {
+        error: null,
+        loading: true
+      })
+    }
+    case ADOPT_CAT_SUCCESS: {
+      return Object.assign({}, state, {
+        data: null,
+        error: null,
+        loading: false
+      })
+    }
+    case ADOPT_CAT_ERROR: {
+      return Object.assign({}, state, {
+        error: action.error,
+        loading: false
+      })
+    }
+    default: {
+      return state;
+    }
+  }
 }
--- src/reducers/dog.js
+++ src/reducers/dog.js
@@ -1,3 +1,12 @@
+import {
+  ADOPT_DOG_ERROR,
+  ADOPT_DOG_SUCCESS,
+  ADOPT_DOG_REQUEST,
+  FETCH_DOG_ERROR,
+  FETCH_DOG_SUCCESS,
+  FETCH_DOG_REQUEST
+} from '../actions/dog';
+
 const initialState = {
   data: null,
   error: null,
@@ -5,5 +14,47 @@ const initialState = {
 };
 
 export const dogReducer = (state=initialState, action) => {
-  return state;
+  switch (action.type) {
+    case FETCH_DOG_REQUEST: {
+      return Object.assign({}, state, {
+        error: null,
+        loading: true
+      });
+    }
+    case FETCH_DOG_SUCCESS: {
+      return Object.assign({}, state, {
+        data: action.dog,
+        error: null,
+        loading: false
+      })
+    }
+    case FETCH_DOG_ERROR: {
+      return Object.assign({}, state, {
+        error: action.error,
+        loading: false
+      })
+    }
+    case ADOPT_DOG_REQUEST: {
+      return Object.assign({}, state, {
+        error: null,
+        loading: true
+      })
+    }
+    case ADOPT_DOG_SUCCESS: {
+      return Object.assign({}, state, {
+        data: null,
+        error: null,
+        loading: false
+      })
+    }
+    case ADOPT_DOG_ERROR: {
+      return Object.assign({}, state, {
+        error: action.error,
+        loading: false
+      })
+    }
+    default: {
+      return state;
+    }
+  }
 }
